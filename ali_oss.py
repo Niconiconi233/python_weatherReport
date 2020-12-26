754e753a928b861c49893636b6d52f95d7957188
Niconiconi233 <39131279+Niconiconi233@users.noreply.github.com>
2020-12-26 16:45:49 +0800

    Update ali_oss.py

--- ali_oss.py
+++ ali_oss.py
@@ -11,8 +11,6 @@ bucket = dir.get('OSSBUCKET')
 
 
 # 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录RAM控制台创建RAM账号。
-print(len(accesskeysecret))
-print(len(accesskey))
 auth = oss2.Auth(accesskey, accesskeysecret)
 # Endpoint以杭州为例，其它Region请按实际情况填写。
 bucket = oss2.Bucket(auth, endpoint, bucket)
