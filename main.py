60a44d938f2381a807c42b7f44086516ba5718e6
yw <1660994874@qq.com>
2020-12-26 16:00:37 +0800

    test

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from weather import Weather
 import os
 
 def main():
-    weather = Weather()
-    weather.work()
+    # weather = Weather()
+    # weather.work()
+    dir = os.environ
+    print(dir)
 
 
 if __name__ == '__main__':
-    dir = os.environ
-    print(dir)
-    # main()
+    main()
